ece19a010e398d3c6aeb04ac2b7e1ae98e4e76d0
deandamanik <deandamanik0706@gmail.com>
2025-12-31 15:45:43 +0800

    style: implement simple branding for verification and reset password emails

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,10 @@
+@props(['url'])
+<tr>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block; text-decoration: none;">
+            <span style="color: #0f172a; font-size: 20px; font-weight: 500; letter-spacing: -0.05em; text-transform:">
+                User Validation System
+            </span>
+        </a>
+    </td>
+</tr>
